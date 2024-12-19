56f4bcca5a945ed13da53d1f2ea77b231458911a
Jatin <jatin281@github.com>
2024-12-19 22:30:12 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MediAlert',
+  webDir: 'dist'
+};
+
+export default config;
